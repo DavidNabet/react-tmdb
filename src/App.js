095ced4f8a19d1530341b3032d4eee3ee7b1ce41
DavidNabet <david97.nabet@gmail.com>
2022-04-30 18:07:38 +0200

    fix vulnerabilities

--- src/App.js
+++ src/App.js
@@ -13,80 +13,80 @@ import "./App.css";
 import { URL, API_KEY } from "./utils";
 
 function App() {
-	const [isLoading, setIsLoading] = useState(true);
-	// Film recherché
-	const [searchTerm, setSearchTerm] = useState("");
-	// Liste de films
-	const [data, setData] = useState([]);
-	// Sélection d'un film
-	const [currentMovie, setCurrentMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  // Film recherché
+  const [searchTerm, setSearchTerm] = useState("");
+  // Liste de films
+  const [data, setData] = useState([]);
+  // Sélection d'un film
+  const [currentMovie, setCurrentMovie] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				let endpoint;
-				// On affiche les films populaires
-				if (!searchTerm) {
-					endpoint = await axios.get(
-						`${URL}movie/popular?api_key=${API_KEY}&language=fr-FR`
-					);
-				} else {
-					// Sinon on recherche un film
-					endpoint = await axios.get(
-						`${URL}search/movie?api_key=${API_KEY}&language=fr-FR&query=${searchTerm
-							.trim()
-							.toLocaleLowerCase()}`
-					);
-				}
-				if (endpoint.status === 200) {
-					setData(endpoint.data.results);
-					// Dès la première actualisation, il vaut mieux afficher le détail du premier film de la liste de films plutôt qu'un message d'erreur
-					setCurrentMovie(endpoint?.data?.results[0]?.id);
-					setIsLoading(false);
-				}
-			} catch (error) {
-				console.error(error.message);
-			}
-		};
-		fetchData();
-	}, [searchTerm]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let endpoint;
+        // On affiche les films populaires
+        if (!searchTerm) {
+          endpoint = await axios.get(
+            `${URL}movie/popular?api_key=${API_KEY}&language=fr-FR`
+          );
+        } else {
+          // Sinon on recherche un film
+          endpoint = await axios.get(
+            `${URL}search/movie?api_key=${API_KEY}&language=fr-FR&query=${searchTerm
+              .trim()
+              .toLocaleLowerCase()}`
+          );
+        }
+        if (endpoint.status === 200) {
+          setData(endpoint.data.results);
+          // Dès la première actualisation, il vaut mieux afficher le détail du premier film de la liste de films plutôt qu'un message d'erreur
+          setCurrentMovie(endpoint?.data?.results[0]?.id);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [searchTerm]);
 
-	return isLoading ? (
-		<Loader />
-	) : (
-		<main>
-			{/* Ici, j'utilise le context pour éviter les prérendus inutiles à chaque chargement des composants */}
-			<MovieContext.Provider value={{ data, currentMovie, setCurrentMovie }}>
-				<div className="container">
-					<div className="container__half">
-						<Search
-							query={searchTerm}
-							handleChange={(e) => {
-								setSearchTerm(e.target.value);
-							}}
-						/>
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <main>
+      {/* Ici, j'utilise le context pour éviter les prérendus inutiles à chaque chargement des composants */}
+      <MovieContext.Provider value={{ data, currentMovie, setCurrentMovie }}>
+        <div className="container">
+          <div className="container__half">
+            <Search
+              query={searchTerm}
+              handleChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+            />
 
-						{Array.isArray(data) && data.length === 0 ? (
-							<div className="error-message">
-								<p>Ce film n'existe pas.</p>
-							</div>
-						) : (
-							<Listing />
-						)}
-					</div>
-					<div className="container__half">
-						{data.length === 0 || currentMovie === null ? (
-							<div className="error-message" style={{ margin: "0 1rem 0" }}>
-								<p>Aucun film n'a été sélectionné</p>
-							</div>
-						) : (
-							<Details />
-						)}
-					</div>
-				</div>
-			</MovieContext.Provider>
-		</main>
-	);
+            {Array.isArray(data) && data.length === 0 ? (
+              <div className="error-message">
+                <p>Ce film n'existe pas.</p>
+              </div>
+            ) : (
+              <Listing />
+            )}
+          </div>
+          <div className="container__half">
+            {data.length === 0 || currentMovie === null ? (
+              <div className="error-message" style={{ margin: "0 1rem 0" }}>
+                <p>Aucun film n'a été sélectionné</p>
+              </div>
+            ) : (
+              <Details />
+            )}
+          </div>
+        </div>
+      </MovieContext.Provider>
+    </main>
+  );
 }
 
 export default App;
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,18 +4,18 @@ export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 // w92, w154, w185, w342, w500, w780, original
 export const POSTER_SIZE = "w185";
 
-export const API_KEY = "37012dd5f665f158bde0e455b8c83ea4";
+export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const formatDate = (n) => {
-	const date = new Date(n);
+  const date = new Date(n);
 
-	if (!n) return "Aucune date de sortie prévue";
+  if (!n) return "Aucune date de sortie prévue";
 
-	return (
-		(date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
-		"/" +
-		(date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) +
-		"/" +
-		date.getFullYear()
-	);
+  return (
+    (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+    "/" +
+    (date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) +
+    "/" +
+    date.getFullYear()
+  );
 };
